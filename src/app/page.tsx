bd4f73d2e751b1eb8500f256f613460cd254d3e5
cyberph0nic <shainah.wada09@gmail.com>
2023-02-09 14:27:50 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import './globals.css';
+
+export default function Home() {
+  return (
+    <>
+      <main className="w-full h-screen bg-slate-800">
+        <section className="font-body text-[128px] font-extrabold text-pink-600">
+          hello
+        </section>
+      </main>
+    </>
+  );
+}
